70946c39be9f1c93fcdf0d33f60caa7df80940ec
Reywam <reywam@mail.ru>
2018-09-20 18:01:11 +0300

    Реализовано бесконечное движение сетки по горизонтали. Удалены старые методы движения сетки. Переименован title в index.html

--- src/VirtualGrid.ts
+++ src/VirtualGrid.ts
@@ -9,6 +9,7 @@ export class VirtualGrid
     private cellSize:number;
     private horizontalLines:Array<Line>;
     private verticalLines:Array<Line>;
+    private rightBorder:number;
 
     private offsetX:number = 0;
     private offsetY:number = 0;
@@ -37,100 +38,43 @@ export class VirtualGrid
         {
             this.verticalLines[i] = new Line(x, 0, x, canvasHeight, this.lineColor, this.lineThickness);
         }
+
+        this.rightBorder = this.cellSize * this.verticalLinesCount;
     }
 
-    public MoveRight(step:number)
+    public Move(movementX:number, movementY:number)
     {
-        this.offsetX += step;
-        for(let line of this.verticalLines)
-        {
-            let currBegin = line.GetBeginPoint();
-            let currEnd = line.GetEndPoint();
-
-            if((currBegin[0] + step) / this.cellSize > this.verticalLinesCount)
-            {
-                line.SetBeginPoint([0, currBegin[1]]);
-                line.SetEndPoint([0, currEnd[1]]);
-            }
-            else
-            {
-                line.SetBeginPoint([currBegin[0] + step, currBegin[1]]);
-                line.SetEndPoint([currEnd[0] + step, currEnd[1]]);
-            }
-        }
-    }
-
-    public MoveLeft(step:number)
-    {
+        console.log(this.offsetX);
+        movementX = -movementX;
+        this.offsetX += movementX;
         if(this.offsetX <= 0)
         {
             this.offsetX = 0;
             return;
         }
 
-        this.offsetX -= step;
         for(let line of this.verticalLines)
         {
             let currBegin = line.GetBeginPoint();
             let currEnd = line.GetEndPoint();
 
-            if(currBegin[0] - step < 0)
+            if(currBegin[0] - movementX < 0)
             {
-                line.SetBeginPoint([this.cellSize * this.verticalLinesCount, currBegin[1]]);
-                line.SetEndPoint([this.cellSize * this.verticalLinesCount, currEnd[1]]);
+                line.SetBeginPoint([this.rightBorder - (movementX - currBegin[0])
+                    , currBegin[1]]);
+                line.SetEndPoint([this.rightBorder - (movementX - currBegin[0])
+                    , currEnd[1]]);
+            }
+            else if((currBegin[0] - movementX) > this.rightBorder)
+            {
+                let overMoveValue:number = (currBegin[0] - movementX) - this.rightBorder;
+                line.SetBeginPoint([overMoveValue, currBegin[1]]);
+                line.SetEndPoint([overMoveValue, currEnd[1]]);
             }
             else
             {
-                line.SetBeginPoint([currBegin[0] - step, currBegin[1]]);
-                line.SetEndPoint([currEnd[0] - step, currEnd[1]]);
-            }
-        }
-    }
-
-    public MoveUp(step:number)
-    {
-        if(this.offsetY <= 0)
-        {
-            this.offsetY = 0;
-            return;
-        }
-        this.offsetY -= step;
-
-        for(let line of this.horizontalLines)
-        {
-            let currBegin = line.GetBeginPoint();
-            let currEnd = line.GetEndPoint();
-
-            if(currBegin[1] - step < 0)
-            {
-                line.SetBeginPoint([currBegin[0], this.cellSize * this.horizontalLinesCount]);
-                line.SetEndPoint([currEnd[0], this.cellSize * this.horizontalLinesCount]);
-            }
-            else
-            {
-                line.SetBeginPoint([currBegin[0], currBegin[1] - step]);
-                line.SetEndPoint([currEnd[0], currEnd[1] - step]);
-            }
-        }
-    }
-
-    public MoveDown(step:number)
-    {
-        this.offsetY += step;
-        for(let line of this.horizontalLines)
-        {
-            let currBegin = line.GetBeginPoint();
-            let currEnd = line.GetEndPoint();
-
-            if(currBegin[1] + step > this.cellSize * this.horizontalLinesCount)
-            {
-                line.SetBeginPoint([currBegin[0], 0]);
-                line.SetEndPoint([currEnd[0], 0]);
-            }
-            else
-            {
-                line.SetBeginPoint([currBegin[0], currBegin[1] + step]);
-                line.SetEndPoint([currEnd[0], currEnd[1] + step]);
+                line.SetBeginPoint([currBegin[0] - movementX, currBegin[1]]);
+                line.SetEndPoint([currEnd[0] - movementX, currEnd[1]]);
             }
         }
     }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import {VirtualGrid} from "./VirtualGrid";
-import {Randomizer} from "./Randomizer";
 
 let canvas = <HTMLCanvasElement> document.getElementById("canvas");
 canvas.width = window.innerWidth;
@@ -8,67 +7,32 @@ canvas.height = window.innerHeight;
 let ctx: CanvasRenderingContext2D = canvas.getContext("2d");
 let grid:VirtualGrid = new VirtualGrid(canvas.width, canvas.height,50, "#00ff00", 2);
 
-let u:boolean = false;
-let d:boolean = false;
-let r:boolean = false;
-let l:boolean = false;
+let mouseBtnPressed:boolean = false;
 
-function setUp()
-{
-    u = true;
-    d = r = l = false;
+function mouseDown(event: MouseEvent): void {
+    mouseBtnPressed = true;
 }
 
-function setDown()
-{
-    d = true;
-    u = r = l = false;
+function mouseUp(event: MouseEvent): void {
+    mouseBtnPressed = false;
 }
 
-function setRight()
+ctx.canvas.addEventListener("mousedown", mouseDown);
+ctx.canvas.addEventListener("mouseup", mouseUp);
+ctx.canvas.addEventListener("mousemove", mouseMove);
+
+function mouseMove(event: MouseEvent): void
 {
-    r = true;
-    d = u = l = false;
+    if(mouseBtnPressed)
+    {
+        grid.Move(event.movementX, event.movementY);
+    }
 }
-
-function setLeft()
-{
-    l = true;
-    d = r = u = false;
-}
-
-let upBtn = document.getElementById("up");
-upBtn.addEventListener("click", (e:Event)=>setUp());
-
-let downBtn = document.getElementById("down");
-downBtn.addEventListener("click", (e:Event)=>setDown());
-
-let rightBtn = document.getElementById("right");
-rightBtn.addEventListener("click", (e:Event)=>setRight());
-
-let leftBtn = document.getElementById("left");
-leftBtn.addEventListener("click", (e:Event)=>setLeft());
 
 function Render()
 {
-    if(u)
-    {
-        grid.MoveUp(1);
-    }
-    else if (d)
-    {
-        grid.MoveDown(1);
-    }
-    else if (l)
-    {
-        grid.MoveLeft(1);
-    }
-    else if (r)
-    {
-        grid.MoveRight(1);
-    }
-    grid.Draw(ctx);
     requestAnimationFrame(Render);
+    grid.Draw(ctx);
 }
 
 Render();
