be6b2ae09d81472b189e7e4140843baaa784f940
Reywam <reywam@mail.ru>
2018-10-01 20:35:09 +0300

    Добавлен контроль FPS для requestAnimationFrame

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,15 @@ canvas.height = window.innerHeight;
 
 let ctx: CanvasRenderingContext2D = canvas.getContext("2d");
 
-let calculationHelper:GridCalculationHelper = new GridCalculationHelper(ctx);
-
-const INITIAL_SHAPE_SIZE:number = 30;
+const INITIAL_SHAPE_SIZE:number = 50;
 const INITIAL_SHAPE_COLOR:string = "#00ff00";
 const CELL_SIZE:number = 100;
 const LINE_COLOR:string = "#000000";
 const BACKGROUND_COLOR:string = "#dee2eb";
 
+let calculationHelper:GridCalculationHelper = new GridCalculationHelper(ctx);
 let shapeCreator:RectangleCreator = new RectangleCreator(INITIAL_SHAPE_SIZE, INITIAL_SHAPE_COLOR);
+
 let grid:VirtualGrid = new VirtualGrid(canvas.width
     , canvas.height
     , CELL_SIZE
@@ -47,10 +47,12 @@ function mouseMove(event: MouseEvent): void
     }
 }
 
-function Render()
-{
-    requestAnimationFrame(Render);
-    grid.Draw(ctx);
+const FPS:number = 60;
+function Render() {
+    setTimeout(function() {
+        requestAnimationFrame(Render);
+        grid.Draw(ctx);
+    }, 1000 / FPS);
 }
 
 Render();
